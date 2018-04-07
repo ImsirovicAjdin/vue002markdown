b04ac59b846020c21a3333540ba83e6f46799b0b
ajdika <ajdika@live.com>
2018-04-07 03:34:27 -0400

    Remove previous commit's notes

--- script.js
+++ script.js
@@ -5,15 +5,6 @@ new Vue({
       content: 'This is a note. You can type in here.',
     }
   },
-  // (1)
-  // A very powerful feature of Vue is the COMPUTED PROPERTY.
-  // It allows us to DEFINE NEW PROPERTIES that COMBINE any # of properties and use TRANSFORMATIONS,
-  // such as converting a markdown string into HTML - that's why IT'S VALUE IS DEFINED by a function.
-  // A computed property has the following features:
-  // - The VALUE IS CACHED
-  // - It is auto-updated as needed WHEN A PROPERTY USED INSIDE THE FUNCTION HAS CHANGED
-  // - It will help us automatically convert the note markdown into valid HTML, so we can display a preview in real time.
-  // - The computed property is DECLARED IN THE COMPUTED OPTION:
   computed: {
     notePreview () {
       // Markdown rendered to HTML 

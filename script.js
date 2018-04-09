3631c8ff9041f707b7b462204db2659ba7ff1822
ajdika <ajdika@live.com>
2018-04-09 00:26:16 -0400

    Remove notes from the previous commit

--- script.js
+++ script.js
@@ -10,16 +10,11 @@ new Vue({
       return marked(this.content);
     },
   },
-    // (2) We can now use the method name in the handler option of our watcher:
   watch: {
     content: {
       handler: 'saveNote',
     },
   },
-  // (1) USING A METHOD
-  // We can write some logic in reusable functions called METHODS.
-  // Let's move our saving logic into one:
-  // Add a new METHODS option to the Vue instance and use the localStorage API there:
   methods: {
     saveNote (val) {
       console.log('saving note', val);

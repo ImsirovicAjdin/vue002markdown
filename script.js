32c3621b08680989ce2b79a0c077663da49ee7cb
ajdika <ajdika@live.com>
2018-04-09 02:05:38 -0400

    Add a method to create a new note, call it addNote - it will create a new note object with a default value - we take the current time, which will be a perfect way to have a unique identifier on each note. We also set default values, such as title and some content, plus the created date and the favorite Boolean. Finally, we add the note to the notes array property.

--- script.js
+++ script.js
@@ -24,5 +24,19 @@ new Vue({
     reportOperation (opName) {
       console.log('The', opName, 'operation was completed!');
     },
+    // Add a note with some default contend and select it
+    addNote () {
+      const time = Date.now();
+      // Default new note
+      const note = {
+        id: String(time),
+        title: 'New note ' + (this.notes.length + 1),
+        content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+        created: time,
+        favorite: false,
+      }
+      // Add to the list
+      this.notes.push(note)
+    },
   },
 })

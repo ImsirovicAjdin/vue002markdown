2227c68f0fcd24f87a32cd7a223d35107b25f3ad
ajdika <ajdika@live.com>
2018-04-09 12:41:41 -0400

    Remove old comments and ADD A NEW COMPUTED property called selectedNote that returns the note with an ID that matches our selectedId property

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ new Vue({
     return {
       content: localStorage.getItem('content') || 'You can write in **markdown**',
       notes: [],
-      // Selectin a note: When a note is selected, it becomes the context of the middle
-      // and right panes of the app -- the text editor modifies its content, and the 
-      // preview pane displays its formatted markdown. Let's implement this behavior:
-      // (1) Id of the selected note:
       selectedId: null,
     }
   },
@@ -15,6 +11,10 @@ new Vue({
     notePreview () {
       return marked(this.content);
     },
+    selectedNote () {
+      // We return the matching note with selectedId
+      return this.notes.find(note => note.id === this.selectedId);
+    }
   },
   watch: {
     content:'saveNote',
@@ -28,10 +28,8 @@ new Vue({
     reportOperation (opName) {
       console.log('The', opName, 'operation was completed!');
     },
-    // Add a note with some default contend and select it
     addNote () {
       const time = Date.now();
-      // Default new note
       const note = {
         id: String(time),
         title: 'New note ' + (this.notes.length + 1),
@@ -39,11 +37,8 @@ new Vue({
         created: time,
         favorite: false,
       }
-      // Add to the list
       this.notes.push(note)
     },
-    // (2) We need a new method that will be called when we click on a note in the list to 
-    // select ID. Let's call it selectNote:
     selectNote (note) {
       this.selectedId = note.id
     },
